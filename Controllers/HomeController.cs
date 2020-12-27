acce56f6af35bcc8ff61726d3a6f59ad2a31fe07
mremre19 <44emreyildiz@gmail.com>
2020-12-27 19:10:16 +0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace koronamorona.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Günlük Veriler.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Covid hakkında yapılması gerekenler.";
+
+            return View();
+        }
+    }
+}
